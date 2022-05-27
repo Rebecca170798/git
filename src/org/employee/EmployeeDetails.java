bf51ba1f0212c2551a6a54457c11608c38d45b9d
Rebecca170798 <reavathyrebecca@gmail.com>
2022-05-27 14:40:52 +0530

    git is a repository

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,36 @@
+package org.employee;
+
+public class EmployeeDetails {
+	
+	private void empDet1() {
+		System.out.println("10");
+
+	}
+
+	private void empDet2() {
+		System.out.println("20");
+
+	}
+	 
+	
+	private void empDet3() {
+		System.out.println("30");
+
+	}
+	
+	public static void main(String[] args) {
+		EmployeeDetails ed = new EmployeeDetails();
+		ed.empDet1();
+		ed.empDet2();
+		ed.empDet3();
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
+
